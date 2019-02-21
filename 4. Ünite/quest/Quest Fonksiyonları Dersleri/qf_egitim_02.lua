2140f79cefe0f59e2fb11899a992d3fcf4cec7a2
MMOTutkunlari <gs.eray.55@hotmail.com>
2019-02-21 15:18:19 +0300

    +quest uygulamaları

---
+++ 4. Ünite/quest/Quest Fonksiyonları Dersleri/qf_egitim_02.lua
@@ -0,0 +1,15 @@
+-- Metin2 Geliþtiricilik Eðitimi | Quest Dersleri | Bölüm 46 ~ 
+-- MMOTutkunlari.com | Whistle | Eray Öksüz
+-- 15.02.2019 - 16.30
+-- Bu dosyanýn videosunu izlemek istiyorsanýz: 
+
+quest qf_egitim_02 begin
+	when letter begin
+		send_letter("Quest Fonksiyonu Eðitimi 2")
+	end
+
+	when button or info begin
+		say_title("Quest Fonksiyonu Eðitimi 2: ")
+		
+	end
+end
